ea9196c7822260296283e74e5ad4e49e591a7c6a
Mark Asdoorian <mark.asdoorian@ocado.com>
2024-05-19 16:26:28 -0700

    fix: removed override of Buffer

--- src/RosLib.js
+++ src/RosLib.js
@@ -3,6 +3,12 @@
  * @author Russell Toris - rctoris@wpi.edu
  */
 
+import * as ActionLib from './actionlib/index.js';
+import * as Core from './core/index.js';
+import * as Math from './math/index.js';
+import * as Tf from './tf/index.js';
+import * as Urdf from './urdf/index.js';
+
 /** @description Library version */
 export const REVISION = '1.4.1';
 export * from './core/index.js';
@@ -10,14 +16,6 @@ export * from './actionlib/index.js';
 export * from './math/index.js';
 export * from './tf/index.js';
 export * from './urdf/index.js';
-
-import * as Core from './core/index.js';
-import * as ActionLib from './actionlib/index.js';
-import * as Math from './math/index.js';
-import * as Tf from './tf/index.js';
-import * as Urdf from './urdf/index.js';
-import { Buffer } from 'buffer'
-globalThis.Buffer = Buffer
 
 // Add to global namespace for in-browser support (i.e. CDN)
 globalThis.ROSLIB = {
