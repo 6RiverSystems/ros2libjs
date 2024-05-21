55d01fbfec6cac65c2e324f70ba72a6e9726eaf5
Mark Asdoorian <mark.asdoorian@ocado.com>
2024-05-21 12:37:15 -0700

    fix: var ref

--- src/actionlib/ActionClient.js
+++ src/actionlib/ActionClient.js
@@ -90,7 +90,7 @@ export default class ActionClient extends EventEmitter {
       }
       // remove the goal if it is cancelled or aborted
       [5, 6].forEach((status) => {
-        if (statusMessage.statusList.includes(status)) {
+        if (statusMessage.status_list.includes(status)) {
           delete this.goals[statusMessage.status.goal_id.id];
         }
       });
